5adb78f80cc5775a084c9bd0b9a443ba199363d7
uyanda-maseti <masetibabalo2@gmail.com>
2025-06-28 17:54:35 +0200

    Create the displayOutput() function

--- main.js
+++ main.js
@@ -4,7 +4,23 @@ function fetchContacts(){
         return response.json()
     })
     .then(function(data){
-        // displayOutput(data)
-        console.log(data)
+        displayOutput(data)
+        // console.log(data)
     })
+}
+
+function displayOutput(data){
+    output = '<table>'
+
+    for(a in data){
+        output += `
+            <tr>
+                <td><img src="${rootPath}controller/uploads/${data[a].avatar}"></td>
+                <td><h5>${data[a].firstname}</h5></td>
+                <td><h5>${data[a].lastname}</h5></td>
+            </tr>
+        `
+    }
+    output += '</table>'
+    document.getElementById('table').innerHTML = output
 }
